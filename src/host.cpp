2ace408317f186ab5314948a25b6ec5cb78d6c8b
Tamas Farkas <farkas.tamas@simonyi.bme.hu>
2019-05-13 16:54:15 +0200

    output fix

--- src/host.cpp
+++ src/host.cpp
@@ -5,48 +5,48 @@
 #include <string.h>
 #include <vector>
 
-#define DATA_SIZE 1024 * 1024 * 32		//2*4*1MB
+#define DATA_SIZE 64 * 32		//2*4*1MB
 
 int main(int argc, char** argv)
 {
-  if (argc != 2) {
-    std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
-    return EXIT_FAILURE;
-  }
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
+		return EXIT_FAILURE;
+	}
 
-  std::string binaryFile = argv[1];
+	std::string binaryFile = argv[1];
 
-  std::cout << "program started" << std::endl;
-  
-  //Allocate Memory in Host Memory
-  size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
+	std::cout << "program started" << std::endl;
 
-  std::vector<int,aligned_allocator<int>> dummydataset1_0(DATA_SIZE);
-  std::vector<int,aligned_allocator<int>> dummydataset1_1(DATA_SIZE);
-  std::vector<int,aligned_allocator<int>> dummydataset1_2(DATA_SIZE);
-  std::vector<int,aligned_allocator<int>> dummydataset1_3(DATA_SIZE);
-  std::vector<int,aligned_allocator<int>> dummydataset2_0(DATA_SIZE);
-  std::vector<int,aligned_allocator<int>> dummydataset2_1(DATA_SIZE);
-  std::vector<int,aligned_allocator<int>> dummydataset2_2(DATA_SIZE);
-  std::vector<int,aligned_allocator<int>> dummydataset2_3(DATA_SIZE);
-  //std::vector<int,aligned_allocator<int>> source_hw_results(DATA_SIZE/32);
-  //std::vector<int,aligned_allocator<int>> source_sw_results(DATA_SIZE/32);
+	//Allocate Memory in Host Memory
+	size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
 
-  std::vector<int,aligned_allocator<int>> source_hw_results(1);
+	std::vector<int,aligned_allocator<int>> dummydataset1_0(DATA_SIZE);
+	std::vector<int,aligned_allocator<int>> dummydataset1_1(DATA_SIZE);
+	std::vector<int,aligned_allocator<int>> dummydataset1_2(DATA_SIZE);
+	std::vector<int,aligned_allocator<int>> dummydataset1_3(DATA_SIZE);
+	std::vector<int,aligned_allocator<int>> dummydataset2_0(DATA_SIZE);
+	std::vector<int,aligned_allocator<int>> dummydataset2_1(DATA_SIZE);
+	std::vector<int,aligned_allocator<int>> dummydataset2_2(DATA_SIZE);
+	std::vector<int,aligned_allocator<int>> dummydataset2_3(DATA_SIZE);
+	//std::vector<int,aligned_allocator<int>> source_hw_results(DATA_SIZE/32);
+	//std::vector<int,aligned_allocator<int>> source_sw_results(DATA_SIZE/32);
 
-  // Create the test data and Software Result
-  for(int i = 0 ; i < DATA_SIZE ; i++){
-    dummydataset1_0[i] = i;
-    dummydataset1_1[i] = i+i;
-    dummydataset1_2[i] = i+i+i;
-    dummydataset1_3[i] = i+i+i+i;
-    dummydataset2_0[i] = i+i+i+i;
-    dummydataset2_1[i] = i+i+i;
-    dummydataset2_2[i] = i+i;
-    dummydataset2_3[i] = i;
-  }
+	std::vector<int,aligned_allocator<int>> source_hw_results(1);
 
-  /*  for(int i = 0; i < DATA_SIZE/32; i++){
+	// Create the test data and Software Result
+	for(int i = 0 ; i < DATA_SIZE ; i++){
+		dummydataset1_0[i] = i;
+		dummydataset1_1[i] = i+i;
+		dummydataset1_2[i] = i+i+i;
+		dummydataset1_3[i] = i+i+i+i;
+		dummydataset2_0[i] = i+i+i+i;
+		dummydataset2_1[i] = i+i+i;
+		dummydataset2_2[i] = i+i;
+		dummydataset2_3[i] = i;
+	}
+
+	/*  for(int i = 0; i < DATA_SIZE/32; i++){
   for(int j = 0 j < DATA_SIZE/32 j+=32)
   for(int k = 0; k < 32;)
   temp_popcount1[n] =  __builtin_popcount(dataset1_0[i+k]);
@@ -56,79 +56,79 @@ int main(int argc, char** argv)
   source_hw_results[i+j] = 0;
 }*/
 
-//OPENCL HOST CODE AREA START
-cl_int err;
-unsigned fileBufSize;
-std::vector<cl::Device> devices = xcl::get_xil_devices();
-cl::Device device = devices[0];
+	//OPENCL HOST CODE AREA START
+	cl_int err;
+	unsigned fileBufSize;
+	std::vector<cl::Device> devices = xcl::get_xil_devices();
+	cl::Device device = devices[0];
 
-OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
-OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
-OCL_CHECK(err, std::string device_name = device.getInfo<CL_DEVICE_NAME>(&err));
+	OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
+	OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+	OCL_CHECK(err, std::string device_name = device.getInfo<CL_DEVICE_NAME>(&err));
 
-char* fileBuf = xcl::read_binary_file(binaryFile, fileBufSize);
-cl::Program::Binaries bins{{fileBuf, fileBufSize}};
-devices.resize(1);
-OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
-OCL_CHECK(err, cl::Kernel krnl_tancalc(program,"tancalc", &err));
+	char* fileBuf = xcl::read_binary_file(binaryFile, fileBufSize);
+	cl::Program::Binaries bins{{fileBuf, fileBufSize}};
+	devices.resize(1);
+	OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
+	OCL_CHECK(err, cl::Kernel krnl_tancalc(program,"tancalc", &err));
 
-//Allocate Buffer in Global Memory
+	//Allocate Buffer in Global Memory
 
-OCL_CHECK(err, cl::Buffer dataset1_0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-  vector_size_bytes, dummydataset1_0.data(), &err));
-  OCL_CHECK(err, cl::Buffer dataset1_1 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-    vector_size_bytes, dummydataset1_1.data(), &err));
-    OCL_CHECK(err, cl::Buffer dataset1_2 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-      vector_size_bytes, dummydataset1_2.data(), &err));
-      OCL_CHECK(err, cl::Buffer dataset1_3 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-        vector_size_bytes, dummydataset1_3.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset1_0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset1_0.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset1_1 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset1_1.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset1_2 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset1_2.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset1_3 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset1_3.data(), &err));
 
-        OCL_CHECK(err, cl::Buffer dataset2_0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-          vector_size_bytes, dummydataset2_0.data(), &err));
-          OCL_CHECK(err, cl::Buffer dataset2_1 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-            vector_size_bytes, dummydataset2_1.data(), &err));
-            OCL_CHECK(err, cl::Buffer dataset2_2 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-              vector_size_bytes, dummydataset2_2.data(), &err));
-              OCL_CHECK(err, cl::Buffer dataset2_3 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-                vector_size_bytes, dummydataset2_3.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset2_0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset2_0.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset2_1 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset2_1.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset2_2 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset2_2.data(), &err));
+	OCL_CHECK(err, cl::Buffer dataset2_3 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			vector_size_bytes, dummydataset2_3.data(), &err));
 
-                /*OCL_CHECK(err, cl::Buffer output0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+	/*OCL_CHECK(err, cl::Buffer output0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
                 vector_size_bytes, source_hw_results.data(), &err));*/
-                OCL_CHECK(err, cl::Buffer output0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
-                  sizeof(int), source_hw_results.data(), &err));
+	OCL_CHECK(err, cl::Buffer output0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+			sizeof(int), source_hw_results.data(), &err));
 
 
-                  int size = DATA_SIZE/32;
+	int size = DATA_SIZE/32;
 
-                  //Set the Kernel Arguments
-                  int arg_index = 0;
+	//Set the Kernel Arguments
+	int arg_index = 0;
 
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_0));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_1));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_2));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_3));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_0));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_1));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_2));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_3));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, output0));
-                  OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, size));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_0));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_1));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_2));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset1_3));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_0));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_1));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_2));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, dataset2_3));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, output0));
+	OCL_CHECK(err, err = krnl_tancalc.setArg(arg_index++, size));
 
-                  //Copy input data to device global memory
-                  OCL_CHECK(err, err = q.enqueueMigrateMemObjects({dataset1_0, dataset1_1, dataset1_2, dataset1_3, dataset2_0, dataset2_1, dataset2_2, dataset2_3},0/* 0 means from host*/));
+	//Copy input data to device global memory
+	OCL_CHECK(err, err = q.enqueueMigrateMemObjects({dataset1_0, dataset1_1, dataset1_2, dataset1_3, dataset2_0, dataset2_1, dataset2_2, dataset2_3},0/* 0 means from host*/));
 
-                  std::cout << "kernel started" << std::endl;
+	std::cout << "kernel started" << std::endl;
 
-                  //Launch the Kernel
-                  OCL_CHECK(err, err = q.enqueueTask(krnl_tancalc));
+	//Launch the Kernel
+	OCL_CHECK(err, err = q.enqueueTask(krnl_tancalc));
 
-                  //Copy Result from Device Global Memory to Host Local Memory
-                  OCL_CHECK(err, err = q.enqueueMigrateMemObjects({output0},CL_MIGRATE_MEM_OBJECT_HOST));
-                  OCL_CHECK(err, err = q.finish());
-                  //OPENCL HOST CODE AREA END
+	//Copy Result from Device Global Memory to Host Local Memory
+	OCL_CHECK(err, err = q.enqueueMigrateMemObjects({output0},CL_MIGRATE_MEM_OBJECT_HOST));
+	OCL_CHECK(err, err = q.finish());
+	//OPENCL HOST CODE AREA END
 
-                  // Compare the results of the Device to the simulation
-                  /*    int match = 0;
+	// Compare the results of the Device to the simulation
+	/*    int match = 0;
                   for (int i = 0 ; i < DATA_SIZE ; i++){
                   if (source_hw_results[i] != source_sw_results[i]){
                   std::cout << "Error: Result mismatch" << std::endl;
@@ -138,10 +138,10 @@ OCL_CHECK(err, cl::Buffer dataset1_0 (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_
                   break;
                 }
               }*/
-              std::cout << "program finished," << " result = " << source_hw_results[0] << std::endl;
+	std::cout << "program finished," << " result = " << source_hw_results[0] << std::endl;
 
-              delete[] fileBuf;
+	delete[] fileBuf;
 
-              //std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
-              return 0;// (match ? EXIT_FAILURE :  EXIT_SUCCESS);
-            }
+	//std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
+	return 0;// (match ? EXIT_FAILURE :  EXIT_SUCCESS);
+}
--- src/tancalc.cl
+++ src/tancalc.cl
@@ -1,5 +1,5 @@
-#define DATA_SIZE 1024 * 1024 * 32
-#define LOCAL_MEM_SIZE 256
+#define DATA_SIZE 64 * 32
+#define LOCAL_MEM_SIZE 16
 
 // Tripcount identifiers
 __constant int c_size = (LOCAL_MEM_SIZE * 2) / 4;
@@ -33,7 +33,7 @@ void vector_to_scalar(ushort16 vector, ushort *scalar){
 	__attribute__((xcl_unroll_hint(16)))
 	__attribute__((xcl_loop_tripcount(16, 16)))
 	for(uchar i = 0; i < 16; i++){
-		scalar +=  vector_data(vector, i);
+		*scalar +=  vector_data(vector, i);
 	}
 }
 
@@ -149,7 +149,7 @@ void ref_read(__global uint16 *dataset1_0, __global uint16 *dataset1_1, __global
 }
 
 
-void cmpr_read0(__global uint16 *dataset2_0,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], ushort cmpr_num){
+void cmpr_read0(__global uint16 *dataset2_0,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], uint cmpr_num){
 	ushort16 temp;
 	if (cmpr_num % 2 == 0) {
 		cmpr_local[0][0] = dataset2_0[cmpr_num];
@@ -164,7 +164,7 @@ void cmpr_read0(__global uint16 *dataset2_0,  uint16 cmpr_local[4][2],  ushort c
 	}
 }
 
-void cmpr_read1(__global uint16 *dataset2_1,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], ushort cmpr_num){
+void cmpr_read1(__global uint16 *dataset2_1,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], uint cmpr_num){
 	ushort16 temp;
 	if (cmpr_num % 2 == 0) {
 		cmpr_local[1][0] = dataset2_1[cmpr_num];
@@ -179,7 +179,7 @@ void cmpr_read1(__global uint16 *dataset2_1,  uint16 cmpr_local[4][2],  ushort c
 	}
 }
 
-void cmpr_read2(__global uint16 *dataset2_2,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], ushort cmpr_num){
+void cmpr_read2(__global uint16 *dataset2_2,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], uint cmpr_num){
 	ushort16 temp;
 	if (cmpr_num % 2 == 0) {
 		cmpr_local[2][0] = dataset2_2[cmpr_num];
@@ -194,7 +194,7 @@ void cmpr_read2(__global uint16 *dataset2_2,  uint16 cmpr_local[4][2],  ushort c
 	}
 }
 
-void cmpr_read3(__global uint16 *dataset2_3,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], ushort cmpr_num){
+void cmpr_read3(__global uint16 *dataset2_3,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], uint cmpr_num){
 	ushort16 temp;
 	if (cmpr_num % 2 == 0) {
 		cmpr_local[3][0] = dataset2_3[cmpr_num];
@@ -209,8 +209,8 @@ void cmpr_read3(__global uint16 *dataset2_3,  uint16 cmpr_local[4][2],  ushort c
 	}
 }
 
-//__attribute__ ((xcl_dataflow))
-void cmpr_read(__global uint16 *dataset2_0, __global uint16 *dataset2_1, __global uint16 *dataset2_2, __global uint16 *dataset2_3,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], ushort cmpr_num){
+__attribute__ ((xcl_dataflow))
+void cmpr_read(__global uint16 *dataset2_0, __global uint16 *dataset2_1, __global uint16 *dataset2_2, __global uint16 *dataset2_3,  uint16 cmpr_local[4][2],  ushort cmprpop_local[4], uint cmpr_num){
 	cmpr_read0(dataset2_0, cmpr_local, cmprpop_local, cmpr_num);
 	cmpr_read1(dataset2_1, cmpr_local, cmprpop_local, cmpr_num);
 	cmpr_read2(dataset2_2, cmpr_local, cmprpop_local, cmpr_num);
@@ -218,18 +218,19 @@ void cmpr_read(__global uint16 *dataset2_0, __global uint16 *dataset2_1, __globa
 }
 
 
-void calculation( uint16 ref_local[LOCAL_MEM_SIZE][2],  uint16 cmpr_local[4][2],  ushort refpop_local[LOCAL_MEM_SIZE],  ushort cmprpop_local[4], int result_local){
+void calculation( uint16 ref_local[LOCAL_MEM_SIZE][2],  uint16 cmpr_local[4][2],  ushort refpop_local[LOCAL_MEM_SIZE],  ushort cmprpop_local[4], int result_local[1]){
 	__attribute__((xcl_pipeline_loop(1)))
 	__attribute__((xcl_loop_tripcount(4, 4)))
 	cmpr_read: for(uchar n = 0; n < 4; n++){
-		__attribute__((xcl_loop_tripcount(1, LOCAL_MEM_SIZE)))
+		__attribute__((opencl_unroll_hint(LOCAL_MEM_SIZE)))
+		__attribute__((xcl_loop_tripcount(LOCAL_MEM_SIZE, LOCAL_MEM_SIZE)))
 		calculation: for(ushort ref_num = 0; ref_num < LOCAL_MEM_SIZE; ref_num++){
 			ushort16 temp;
 			ushort temp2 = 0;
 			temp = convert_ushort16(popcount(ref_local[ref_num][0] & cmpr_local[n][0]) + popcount(ref_local[ref_num][1] & cmpr_local[n][1]));
 			vector_to_scalar(temp, &temp2);
-			if(temp2 <= (refpop_local[ref_num]  + cmprpop_local[n]  - temp2)){
-				result_local++;
+			if(temp2 >= (refpop_local[ref_num]  + cmprpop_local[n]  - temp2)){
+				result_local[0]++;
 			}
 		}
 	}
@@ -247,19 +248,19 @@ void tancalc(__global uint16 *dataset1_0, __global uint16 *dataset1_1, __global 
 		ushort refpop_local[LOCAL_MEM_SIZE];__attribute__((xcl_array_partition(complete, 0)))
 		uint16 cmpr_local[4][2];__attribute__((xcl_array_partition(complete, 0)))
 		ushort cmprpop_local[4];__attribute__((xcl_array_partition(complete, 0)))
-		int result_local = 0;
+		int result_local[] = {0};
 
-		ushort size_in = size * 2;
+		uint size_in = size * 2;
 
-		__attribute__((xcl_loop_tripcount(c_size_in/4, c_size_in/4)))
+		__attribute__((xcl_loop_tripcount(c_size_in/c_size, c_size_in/c_size)))
 		mainloop: for (ushort chunk_num = 0; chunk_num < size_in; chunk_num += c_size) {
 
 			//read ref until mem is full and popcount
 			ref_read(dataset1_0, dataset1_1, dataset1_2, dataset1_3, ref_local, refpop_local, chunk_num);
 
-			__attribute__((xcl_pipeline_loop(1)))
+			//__attribute__((xcl_pipeline_loop(1)))
 			__attribute__((xcl_loop_tripcount(c_size_in, c_size_in)))
-			cmprmain: for(ushort cmpr_num = 0; cmpr_num < size_in; cmpr_num++){
+			cmprmain: for(uint cmpr_num = 0; cmpr_num < size_in; cmpr_num++){
 				cmpr_read(dataset2_0, dataset2_1, dataset2_2, dataset2_3, cmpr_local, cmprpop_local, cmpr_num);
 				if (cmpr_num % 2 == 1) {
 					calculation(ref_local, cmpr_local, refpop_local, cmprpop_local, result_local);
@@ -268,6 +269,6 @@ void tancalc(__global uint16 *dataset1_0, __global uint16 *dataset1_1, __global 
 		}
 
 		//-----
-		output0[0] = result_local;
+		output0[0] = result_local[0];
 		//-----
 	}
